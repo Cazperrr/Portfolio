8a712b0dc6729523cff0378e382182d42d80e59a
Cazper <cazper>
2026-03-11 19:05:18 +0100

    Added weather API feature

--- index.js
+++ index.js
@@ -5,3 +5,55 @@ window.addEventListener("scroll", function () {
         document.getElementById("navbar").style.boxShadow = "none";
     }
 });
+
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+const apiKey = "63a8a3fd2de25d3d346a29994d7cbe3d";
+const city = "Gothenburg";
+const url = `${apiUrl}?q=${city}&appid=${apiKey}&units=metric&lang=sv`;
+
+//test: https://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=63a8a3fd2de25d3d346a29994d7cbe3d&units=metric&lang=sv
+
+fetch(url)
+    .then((response) => {
+        if (!response.ok) {
+            const weatherBox = document.querySelector('.weather-box');
+            weatherBox.style.display = 'none';
+
+            throw new Error("Nätverksrespons var inte ok");
+        }
+        return response.json();
+    })
+    .then((data) => {
+        const temprature = data.main.temp;
+        const location = data.name;
+        const image = document.querySelector('.weather-box img');
+        document.getElementById("weatherInfo").innerHTML = `${location} ${temprature}°C.`;
+
+        switch (data.weather[0].main) {
+            case 'Clear':
+                image.src = 'images/Clear.png';
+                break;
+
+            case 'Clouds':
+                image.src = 'images/Cloud.png';
+                break;
+
+            case 'Mist':
+                image.src = 'images/Mist.png';
+                break;
+
+            case 'Rain':
+                image.src = 'images/Rain.png';
+                break;
+
+            case 'Snow':
+                image.src = 'images/Snow.png';
+                break;
+
+            default:
+                image.src = '';
+        }
+    })
+    .catch((error) => {
+        console.error("Det gick inte att hämta väderdata:", error)
+    });
